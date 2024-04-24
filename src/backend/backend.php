584a17e2eca6169ebefac71e5df13f5cdeccbee7
MarcelHasenmaierPlanB <marcel.hasenmaier@plan-b-gmbh.com>
2024-04-24 17:04:09 +0200

    set up backend and todo file

---
+++ src/backend/backend.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>backend</title>
+</head>
+<body>
+    
+    <?php
+
+    $intZahl = 5;
+    $charText = "Hallo!";
+
+    echo nl2br ("Zahl: " .$intZahl ."\n");
+    echo "Text: " .$charText;
+
+    ?>
+
+</body>
+</html>
---
+++ todo.md
@@ -0,0 +1,9 @@
+# Todo
+
+1. php-Konsolenausgabe
+2. Verbindung zur Datenbank
+    2.1 GET requests
+    2.2 POST und UPDATE requests
+    2.3 usw.
+3. JS-php Kommunikation
+4. Durchstich
