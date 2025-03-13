fcc77da2637c7c1eed32849443808a76a0b4242f
Marcel "Ody" Hasenmaier <mhasenmaier.git@gmail.com>
2025-03-13 13:58:41 +0100

    what is done since last commit? - todo.html/tasklist scroll if >3 attached tasks - todo.html/addNewTask button
    Todos left:
    - use mockdata to open tasks at todo.html/tasks
    - save data for new todos
    - save data for new tasks
    - todo.html ge rid of the charakter (V and A) and use chevrons for open and close a window/dropdown
    - archiv.html/deleted.html/task.html need at least mock functions
    - links dont "remember" the id of the todo which was clicked
    - links for new todo / already existed todo open the same page and page isnt filled with information from already existed todo
    - rework js
    - reorganise backend code and move functionality from frontend to backend
    - make the code more maintainable
    
    --- on hold ---
    1. create php functions for set up todo, task and link tables if not existing
    2. connect IDE to DB
    3. frontend: the asynchronous activate-button-and-change-page-to-requested-data doesn't work.
     3.1 [DONE] get the id of the parent-div of the clicked button
     3.2 [DONE] save the id
     3.3 [DONE] change the page
     3.4 [DONE] get the data of the id-todo from DB
     3.5 show the data at the new page
    4. Add functionalities for task handling
    5. check the variables at the routing.php switch-cases and if-else: are they empty / not existent or at a wrong datatype?
    6. update function should just update

--- src/website/script.js
+++ src/website/script.js
@@ -113,6 +113,128 @@ const mockXMLData = `<todos>
     </todo>
 </todos>`;
 
+const mochXmlDataTask = <tasks>
+    <task>
+        <id>1</id>
+        <title>Task Titel 1</title>
+        <description>Eine kreative Beschreibung für Task 1</description>
+        <status>3</status>
+        <todo>1</todo>
+    </task>
+    <task>
+        <id>2</id>
+        <title>Task Titel 2</title>
+        <description>Beschreibung für Task 2: Hier könnte Ihre Werbung stehen!</description>
+        <status>1</status>
+        <todo>1</todo>
+    </task>
+    <task>
+        <id>3</id>
+        <title>Task Titel 3</title>
+        <description>Task 3: Machen Sie sich bereit für Abenteuer!</description>
+        <status>5</status>
+        <todo>2</todo>
+    </task>
+    <task>
+        <id>4</id>
+        <title>Task Titel 4</title>
+        <description>Für Task 4: Genießen Sie den Moment!</description>
+        <status>2</status>
+        <todo>2</todo>
+    </task>
+    <task>
+        <id>5</id>
+        <title>Task Titel 5</title>
+        <description>Beschreibung für Task 5: Ein Schritt nach dem anderen.</description>
+        <status>4</status>
+        <todo>1</todo>
+    </task>
+    <task>
+        <id>6</id>
+        <title>Task Titel 6</title>
+        <description>Task 6: Der frühe Vogel fängt den Wurm.</description>
+        <status>3</status>
+        <todo>3</todo>
+    </task>
+    <task>
+        <id>7</id>
+        <title>Task Titel 7</title>
+        <description>Für Task 7: Vertrauen ist der Schlüssel.</description>
+        <status>5</status>
+        <todo>3</todo>
+    </task>
+    <task>
+        <id>8</id>
+        <title>Task Titel 8</title>
+        <description>Task 8: Kleine Dinge machen einen großen Unterschied.</description>
+        <status>2</status>
+        <todo>3</todo>
+    </task>
+    <task>
+        <id>9</id>
+        <title>Task Titel 9</title>
+        <description>Für Task 9: Denken Sie groß, handeln Sie klein.</description>
+        <status>1</status>
+        <todo>3</todo>
+    </task>
+    <task>
+        <id>10</id>
+        <title>Task Titel 10</title>
+        <description>Task 10: Erfolg ist kein Zufall.</description>
+        <status>4</status>
+        <todo>1</todo>
+    </task>
+    <task>
+        <id>11</id>
+        <title>Task Titel 11</title>
+        <description>Für Task 11: Bleiben Sie neugierig!</description>
+        <status>3</status>
+        <todo>1</todo>
+    </task>
+    <task>
+        <id>12</id>
+        <title>Task Titel 12</title>
+        <description>Task 12: Lernen Sie aus Ihren Fehlern.</description>
+        <status>5</status>
+        <todo>1</todo>
+    </task>
+    <task>
+        <id>13</id>
+        <title>Task Titel 13</title>
+        <description>Für Task 13: Teilen Sie Ihr Wissen.</description>
+        <status>2</status>
+        <todo>5</todo>
+    </task>
+    <task>
+        <id>14</id>
+        <title>Task Titel 14</title>
+        <description>Task 14: Glauben Sie an sich selbst.</description>
+        <status>1</status>
+        <todo>10</todo>
+    </task>
+    <task>
+        <id>15</id>
+        <title>Task Titel 15</title>
+        <description>Für Task 15: Bleiben Sie flexibel.</description>
+        <status>4</status>
+        <todo>10</todo>
+    </task>
+    <task>
+        <id>16</id>
+        <title>Task Titel 16</title>
+        <description>Task 16: Gemeinsam sind wir stark.</description>
+        <status>3</status>
+        <todo>10</todo>
+    </task>
+    <task>
+        <id>17</id>
+        <title>Task Titel 17</title>
+        <description>Für Task 17: Der Weg ist das Ziel.</description>
+        <status>5</status>
+        <todo>10</todo>
+    </task>
+</tasks>
+
 // starts with all html pages
 document.addEventListener('DOMContentLoaded', domContentLoaded);
 
@@ -376,7 +498,7 @@ function isStatusAllowed(todoOrTask, elementStatus) {
     return true;
 }
 
-function saveStatus (todoOrTask, elementStatus) {
+function saveStatus(todoOrTask, elementStatus) {
     let statusValue = statusPopupTodo.options[statusPopupTodo.selectedIndex].value;
     console.log("Status is: " + statusValue);
 
@@ -389,11 +511,11 @@ function saveStatus (todoOrTask, elementStatus) {
     saveTodo();
 }
 
-function saveTodo () {
+function saveTodo() {
     console.log("todo will be saved ... soon");
 
-console.log("todoTitleInput.value = " + todoTitleInput.value);
-console.log("todoDescriptionTextarea.value = " + todoDescriptionTextarea.value);
+    console.log("todoTitleInput.value = " + todoTitleInput.value);
+    console.log("todoDescriptionTextarea.value = " + todoDescriptionTextarea.value);
 
 }
 
